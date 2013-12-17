bd26c06ce3483ef2e2ebd9d0b006c402cfd5aac2
johnnymo87 <johnnymo87@gmail.com>
2013-12-17 02:39:06 +0000

    upgraded server to exportable file, upgraded test to call it

--- test/server_test.js
+++ test/server_test.js
@@ -1,26 +1,31 @@
 var assert = require('assert'),
-    http  = require('http'),
+    http = require('http'),
     server = require('../lib/server');
 
-describe('/', function() {
-    it('should return 200', function(done) {
-        http.get('http://localhost:8888', function(res) {
+before(function () {
+    server.start();
+});
+
+describe('/', function () {
+
+    it('should return 200', function (done) {
+        http.get('http://localhost:8888', function (res) {
             debugger;
             assert.equal(200, res.statusCode);
             done();
         });
     });
 
-    it('should say "Hello, world!"', function(done) {
-        http.get('http://localhost:8888', function(res) {
+    it('should say "Hello, world!"', function (done) {
+        http.get('http://localhost:8888', function (res) {
             var data = '';
 
-            res.on('data', function(chunk) {
+            res.on('data', function (chunk) {
                 data += chunk;
             });
 
-            res.on('end', function() {
-                assert.equal('Hello, world!\n', data);
+            res.on('end', function () {
+                assert.equal('Hello World', data);
                 done();
             });
         });
