b1698b970604689494f87da6e1cb8f181f4945f7
johnnymo87 <johnnymo87@gmail.com>
2013-12-17 02:11:02 +0000

    basic test coverage

--- test/server_test.js
+++ test/server_test.js
@@ -1,4 +1,28 @@
-var assert          = require('assert'),
-    sinon           = require('sinon'),
-    should          = require('should'),
-    server = require('../lib/server.js');
+var assert = require('assert'),
+    http  = require('http'),
+    server = require('../lib/server');
+
+describe('/', function() {
+    it('should return 200', function(done) {
+        http.get('http://localhost:8888', function(res) {
+            debugger;
+            assert.equal(200, res.statusCode);
+            done();
+        });
+    });
+
+    it('should say "Hello, world!"', function(done) {
+        http.get('http://localhost:8888', function(res) {
+            var data = '';
+
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                assert.equal('Hello, world!\n', data);
+                done();
+            });
+        });
+    });
+});
